37140b8a05fcdb94662e6a57086927441197953b
tgts_root <tgts.root@wisseninfotech.com>
2025-06-09 17:46:07 +0530

    Added API Gateway with Microservices

---
+++ section9/cloudgateway/src/main/java/com/example/demo/filters/FilterUtility.java
@@ -0,0 +1,9 @@
+package com.example.demo.filters;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FilterUtility {
+
+	public static final String CORRELATION_ID = "bank-correlation-id";
+}
