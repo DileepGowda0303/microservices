3c06a7bc36744673c187053012a3c8169b89f7ca
dileepgowda8189 <dileepgowda8189@gmail.com>
2025-07-14 11:56:05 +0530

    Oauth2 with Role Based Authorization

--- section12/cloudgateway/src/main/java/com/example/demo/config/SecurityConfig.java
+++ section12/cloudgateway/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,27 +2,35 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
-	    return httpSecurity
-	        .authorizeExchange(exchanges -> exchanges
-	            .pathMatchers(HttpMethod.GET, "/**").permitAll()  // ← Allow all GETs
-	            .pathMatchers("/bank/accounts/**").authenticated()  // ← Stricter paths
-	            .anyExchange().denyAll()  // Deny others (optional)
-	        )
-	        .csrf(csrf -> csrf.disable())
-	        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-	        .build();
+		httpSecurity
+				.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET, "/**").permitAll()
+						.pathMatchers("/bank/accounts/**").hasRole("Accountss"))
+				.oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+						.jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
+		httpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+		return httpSecurity.build();
 	}
 
-
+	private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+		return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+	}
 }
